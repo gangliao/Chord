081906f9b80037a7c22c787881c3298027450a18
Gang Liao <gangliao@cs.umd.edu>
2019-10-04 11:37:56 -0400

    parse r

--- chord/main.cc
+++ chord/main.cc
@@ -11,7 +11,7 @@ void init_node(const cxxopts::ParseResult& result, chord::Node* node) {
         << "Invalid IPv4 address";
     node->address.sin_family = AF_INET;
 
-    int32_t port = result["p"].as<int32_t>();
+    int16_t port = result["p"].as<int16_t>();
     CHECK_GE(port, 1024) << "Invalid option for a port, must be greater than or equal to 1024";
     CHECK_LE(port, 65535) << "Invalid option for a port, must be less than or equal to 65535";
     node->address.sin_port = htons(port);
@@ -20,7 +20,7 @@ void init_node(const cxxopts::ParseResult& result, chord::Node* node) {
         << "Invalid IPv4 address";
     node->join_address.sin_family = AF_INET;
 
-    port = result["jp"].as<int32_t>();
+    port = result["jp"].as<int16_t>();
     CHECK_GE(port, 1024) << "Invalid option for a port, must be greater than or equal to 1024";
     CHECK_LE(port, 65535) << "Invalid option for a port, must be less than or equal to 65535";
     node->join_address.sin_port = htons(port);
@@ -44,6 +44,13 @@ void init_node(const cxxopts::ParseResult& result, chord::Node* node) {
     CHECK_LE(tcp, 60000)
         << "The time in milliseconds between invocations of 'check predecessor' must be less than or equal to 60000";
     node->tv_check_predecessor = tcp * 1.0e-3;
+
+    int32_t r = result["r"].as<int32_t>();
+    CHECK_GE(r, 1)
+        << "The number of successors maintained must be must be greater than or equal to 1";
+    CHECK_LE(r, 32)
+        << "The number of successors maintained must be must be less than or equal to 32";
+    node->r = r;
 }
 
 int main(int argc, char* argv[]) {
@@ -52,9 +59,9 @@ int main(int argc, char* argv[]) {
     // clang-format off
     options.allow_unrecognised_options().add_options("Chord")
         ("a,addr",  "The IPv4 address to bind to and advertise to other nodes", cxxopts::value<std::string>()->default_value("127.0.0.1"))
-        ("p,port",  "The port to bind to (required)", cxxopts::value<int32_t>())
+        ("p,port",  "The port to bind to (required)", cxxopts::value<int16_t>())
         ("ja",      "The IPv4 address of a node whose ring to join", cxxopts::value<std::string>()->default_value("127.0.0.1"))
-        ("jp",      "The port of a node whose ring to join (required)", cxxopts::value<int32_t>())
+        ("jp",      "The port of a node whose ring to join (required)", cxxopts::value<int16_t>())
         ("ts",      "The time in milliseconds between invocations of 'stabilize'", cxxopts::value<int32_t>()->default_value("100"))
         ("tff",     "The time in milliseconds between invocations of 'fix fingers'", cxxopts::value<int32_t>()->default_value("100"))
         ("tcp",     "The time in milliseconds between invocations of 'check predecessor'", cxxopts::value<int32_t>()->default_value("100"))
